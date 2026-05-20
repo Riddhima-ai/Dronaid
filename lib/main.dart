d3c6e2d96666ee37a897f9bfbac7d49a7e28da4d
bhavana-panuganti <bhavana.ram.panuganti@gmail.com>
2026-05-20 17:01:13 +0530

    added checklist

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:live_loc/live_location.dart';
 import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';  
+import 'package:firebase_core/firebase_core.dart';
+import 'checklist.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(
-     MyApp()
-    );
+
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool isFinished = false;
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MapPage(),
+
+      home:ElectronicsChecklistPage()
     );
   }
 }
